0272831e61f2acd0779a3206734ae59893108c77
motunikomi <hasimoto910@gmail.com>
2020-05-03 21:29:33 +0900

    Initialization

---
+++ src/main/java/com/drug/DrugmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.drug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrugmanagementApplication.class, args);
+	}
+
+}
