a90c6240e0834e65785011557e0dac1b59782977
motunikomi <hasimoto910@gmail.com>
2020-05-17 16:26:55 +0900

    set

--- src/main/java/com/drug/DrugmanagementApplication.java
+++ src/main/java/com/drug/DrugmanagementApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DrugmanagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DrugmanagementApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DrugmanagementApplication.class, args);
+  }
 
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,27 @@
-create table member
-(
-    id varchar not null
-        constraint member_pk
-            primary key auto_increment,
-    name varchar not null
+/*CREATE TABLE IF NOT EXISTS item (
+  id 				int(20) 	PRIMARY KEY AUTO_INCREMENT,
+  basket_no 		varchar(64),
+  rack_chemical_id 	int(20),
+  purchase_date 	DATE,
+  disposal_date		DATE,
+  combined_date 	DATE,
+  remarks 			varchar(64),
+);
+
+CREATE TABLE IF NOT EXISTS rack_chemical (
+  id int(20) PRIMARY KEY AUTO_INCREMENT,
+  chemical_id int(20),
+  count int(20),
+  quantity int(20),
+  remaining_amount int(20),
+  status varchar(64),
+);
+*/
+CREATE TABLE IF NOT EXISTS chemical (
+  name varchar(64) PRIMARY KEY,
+  chemical_id int(20),
+  count int(20),
+  quantity int(20),
+  remaining_amount int(20),
+  status varchar(64),
 );
