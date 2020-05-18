67475372e9fd74a7bf2d300a701265c9e37d827e
motunikomi <hasimoto910@gmail.com>
2020-05-19 02:41:19 +0900

    no message

--- src/test/java/com/drug/ItemServiceTests.java
+++ src/test/java/com/drug/ItemServiceTests.java
@@ -2,6 +2,7 @@ package com.drug;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class ItemServiceTests {
@@ -10,6 +11,7 @@ class ItemServiceTests {
   @BeforeEach
   void setUp() throws Exception {}
 
+  @Disabled
   @Test
   void test() {
     fail("まだ実装されていません");
