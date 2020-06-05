fd47cb6795c3d58087b21b3d386a9996c3fd3df2
motunikomi <hasimoto910@gmail.com>
2020-06-05 13:07:44 +0900

    no message

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,36 +1,11 @@
-CREATE TABLE IF NOT EXISTS item (
+
+
+/*
+ * CREATE TABLE IF NOT EXISTS item (
   id int(20) NOT NULL AUTO_INCREMENT,
   name varchar(255),
   price real,
   vendor varchar(255),
   PRIMARY KEY (id)
 );
-
-/*CREATE TABLE IF NOT EXISTS item (
-  id 				int(20) 	PRIMARY KEY AUTO_INCREMENT,
-  basket_no 		varchar(64),
-  rack_chemical_id 	int(20),
-  purchase_date 	DATE,
-  disposal_date		DATE,
-  combined_date 	DATE,
-  remarks 			varchar(64),
-);
-
-CREATE TABLE IF NOT EXISTS rack_chemical (
-  id int(20) PRIMARY KEY AUTO_INCREMENT,
-  chemical_id int(20),
-  count int(20),
-  quantity int(20),
-  remaining_amount int(20),
-  status varchar(64),
-);
-CREATE TABLE IF NOT EXISTS chemical (
-  name varchar(64) PRIMARY KEY,
-  chemical_id int(20),
-  count int(20),
-  quantity int(20),
-  remaining_amount int(20),
-  status varchar(64),
-);
-
-*/
+ */
